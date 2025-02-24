b5a143592b5630e84dad27ac36ecf7f89d10284c
Fed30 <136997447+Fed30@users.noreply.github.com>
2025-02-24 16:28:03 +0000

    update4
    update4

--- interview_simulator/backend/app.py
+++ interview_simulator/backend/app.py
@@ -1,37 +1,16 @@
-import os
-from datetime import timedelta
-from dotenv import load_dotenv
-
-
-
-# Add the config path to the system path
-#sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), 'backend', 'config')))
-
 
 from flask import Flask
 from flask_session import Session
 from flask_cors import CORS
 #from config.app_config import configure_app
+from config import app_config
 from routes import register_routes
-
-# temporary
-load_dotenv()
-
-def configure_app(app):
-    app.config['SESSION_TYPE'] = 'filesystem'
-    app.config['SESSION_PERMANENT'] = True
-    app.config['SESSION_COOKIE_NAME'] = 'my_flask_session'
-    app.config['SESSION_COOKIE_SAMESITE'] = 'None'
-    app.config['SESSION_COOKIE_SECURE'] = True
-    app.secret_key = os.urandom(24)
-    app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=40)  # 40 minutes
-
 
 # Initialize the Flask app
 app = Flask(__name__)
 
 # Configure app settings
-#configure_app(app)
+app_config(app)
 
 # Initialize session and CORS
 Session(app)
---
+++ interview_simulator/backend/config/__init__.py
@@ -0,0 +1,3 @@
+from .app_config import configure_app
+from .firebase_config import firebase_config, firebase_admin_sdk
+from .openai_config import openai
