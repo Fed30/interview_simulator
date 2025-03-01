3d5b656a37a496c87e93a341e94260d3ba0c1820
Fed30 <136997447+Fed30@users.noreply.github.com>
2025-03-01 15:32:37 +0000

    u
    u

--- interview_simulator/src/app/not-found.js
+++ interview_simulator/src/app/not-found.js
@@ -7,7 +7,7 @@ import Link from "next/link";
 export default function NotFound() {
   return (
     <div className="not-found-container">
-      <h1 className="lg:text-3xl text-2xl font-semibold text-transparent bg-clip-text bg-gradient-to-r from-[#F25E86] to-[#6D81F2] relative mt-11">
+      <h1 className="lg:text-3xl text-2xl font-semibold text-transparent bg-clip-text bg-gradient-to-r from-[#F25E86] to-[#6D81F2] relative mt-16">
         404 - Page Not Found
       </h1>
       <Image
@@ -15,7 +15,7 @@ export default function NotFound() {
         width={100}
         height={100}
         alt="Page not found"
-        className="w-3/4 sm:w-2/3 object-contain"
+        className="sm:w-2/3 image-hover"
       />
       <p className="text-lg mt-4 text-white opacity-90 text-shadow-md text-animated animate__animated animate__fadeIn animate__delay-1s">
         Oops! The page you are looking for does not exist.
