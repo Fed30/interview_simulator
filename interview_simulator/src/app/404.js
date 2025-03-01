0697303b453dcef5f15233d2a9735e6f38198ea8
Fed30 <136997447+Fed30@users.noreply.github.com>
2025-03-01 15:16:40 +0000

    u
    u

--- interview_simulator/src/app/404.js
+++ interview_simulator/src/app/404.js
@@ -5,7 +5,9 @@ import Image from "next/image";
 export default function Custom404() {
   return (
     <div className="not-found-container">
-      <h1>404 - Page Not Found</h1>
+      <h1 className="lg:text-3xl text-2xl font-semibold text-transparent bg-clip-text bg-gradient-to-r from-[#F25E86] to-[#6D81F2] relative pb-2">
+        404 - Page Not Found
+      </h1>
       <Image
         src="/404.png"
         width={400}
