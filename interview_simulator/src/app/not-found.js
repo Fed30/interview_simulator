bbfbc6e49e42821e809c9e37ffab79a0dab61231
Fed30 <136997447+Fed30@users.noreply.github.com>
2025-03-01 15:27:11 +0000

    u
    u

--- interview_simulator/src/app/not-found.js
+++ interview_simulator/src/app/not-found.js
@@ -6,18 +6,18 @@ import Link from "next/link";
 
 export default function NotFound() {
   return (
-    <div className="not-found-container text-center">
-      <h1 className="lg:text-4xl text-2xl font-semibold text-transparent bg-clip-text bg-gradient-to-r from-[#F25E86] to-[#6D81F2] relative pb-2">
+    <div className="not-found-container">
+      <h1 className="lg:text-3xl text-2xl font-semibold text-transparent bg-clip-text bg-gradient-to-r from-[#F25E86] to-[#6D81F2] relative mt-9">
         404 - Page Not Found
       </h1>
       <Image
         src="/404.png"
-        width={400}
-        height={400}
+        width={200}
+        height={200}
         alt="Page not found"
-        className="w-3/4 sm:w-2/3 h-auto object-contain"
+        className="w-3/4 sm:w-2/3 object-contain"
       />
-      <p className="text-lg mt-4 opacity-90 text-shadow-md text-animated animate__animated animate__fadeIn animate__delay-1s">
+      <p className="text-lg mt-4 text-white opacity-90 text-shadow-md text-animated animate__animated animate__fadeIn animate__delay-1s">
         Oops! The page you are looking for does not exist.
       </p>
       <Link href="/" className="home-link mt-4">
