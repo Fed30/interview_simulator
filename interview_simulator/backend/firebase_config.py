1d8ce0051b26e71127d747ada92771206fa647e7
Fed30 <136997447+Fed30@users.noreply.github.com>
2025-03-31 18:55:14 +0100

    c
    c

--- interview_simulator/backend/firebase_config.py
+++ interview_simulator/backend/firebase_config.py
@@ -1,9 +1,20 @@
 import firebase_admin
 from firebase_admin import credentials, firestore, db, auth,  storage
 import os
+import json
 
 # Initialize Firebase Admin SDK with your service account
-cred = credentials.Certificate(os.getenv("FIREBASE_CREDENTIALS"))
+# Load Firebase credentials from environment variable
+firebase_credentials = os.getenv("FIREBASE_CREDENTIALS")
+
+if not firebase_credentials:
+    raise ValueError("Missing FIREBASE_CREDENTIALS environment variable")
+
+# Parse the JSON string into a Python dictionary
+firebase_credentials_dict = json.loads(firebase_credentials)
+
+cred = credentials.Certificate(firebase_credentials_dict)
+
 firebase_admin.initialize_app(cred, {
     'databaseURL': 'https://ai-simulator-8cb2e-default-rtdb.firebaseio.com',  # Realtime Database URL
     'projectId': 'ai-simulator-8cb2e',
